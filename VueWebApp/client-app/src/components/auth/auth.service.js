fff31aa92c4759bc698ffedf775dbb9b99e647b2
shamonj03 <shamonj03@users.noreply.github.com>
2021-08-13 18:52:47 -0500

    auth

---
+++ VueWebApp/client-app/src/components/auth/auth.service.js
@@ -0,0 +1,35 @@
+import { UserManager, WebStorageStateStore } from 'oidc-client';
+export default class AuthService {
+    userManager;
+    constructor() {
+        const STS_DOMAIN = 'https://localhost:5001';
+        const settings = {
+            userStore: new WebStorageStateStore({ store: window.localStorage }),
+            authority: STS_DOMAIN,
+            client_id: 'vuejs_code_client',
+            redirect_uri: 'https://localhost:44363/callback.html',
+            automaticSilentRenew: true,
+            silent_redirect_uri: 'https://localhost:44363/silent-renew.html',
+            response_type: 'code',
+            scope: 'openid profile',
+            post_logout_redirect_uri: 'https://localhost:44363/',
+            filterProtocolClaims: true,
+        };
+        this.userManager = new UserManager(settings);
+    }
+    getUser() {
+        return this.userManager.getUser();
+    }
+    login() {
+        return this.userManager.signinRedirect();
+    }
+    logout() {
+        return this.userManager.signoutRedirect();
+    }
+    getAccessToken() {
+        return this.userManager.getUser().then((data) => {
+            return data.access_token;
+        });
+    }
+}
+//# sourceMappingURL=auth.service.js.map
